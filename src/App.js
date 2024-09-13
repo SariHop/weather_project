fe1c84500fe1d792bb536039dd3e706783eebcc4
Sari <sarihop12@gmail.com>
2024-09-13 12:30:04 +0300

    Replace dummy data with real API data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import WeatherData from './components/weatherData';
 
 function App() {
-  const coord = [{lat:0, lon:0}, {lat:0, lon:0}, {lat:0, lon:0}, {lat:0, lon:0}]
+  const coord = [{lat:29.55, lon:34.95},{lat:51.50, lon:-0.12},{lat:40.71, lon:-74.00},{lat:64.20, lon:-149.49}]
 
   return (
     <div className="App">
--- src/components/weatherData.js
+++ src/components/weatherData.js
@@ -1,50 +1,45 @@
 import { useEffect, useState } from 'react'
 import CardTemplate from './template'
+import axios from 'axios'
 
 const WeatherData = ({ coord }) => {
 
-    const [cityData, setCityData] = useState(
-        {   
-            //demo data
-            "coord": { "lon": 10.99, "lat": 44.34 },
-            "weather": [{ "id": 501, "main": "Rain", "description": "moderate rain", "icon": "10d" }],
-            "base": "stations",
-            "main": { "temp": 298.48, "feels_like": 298.74, "temp_min": 297.56, "temp_max": 300.05, "pressure": 1015, "humidity": 64, "sea_level": 1015, "grnd_level": 933 },
-            "visibility": 10000, "wind": { "speed": 0.62, "deg": 349, "gust": 1.18 },
-            "rain": { "1h": 3.16 },
-            "clouds": { "all": 100 },
-            "dt": 1661870592,
-            "sys": { "type": 2, "id": 2075663, "country": "IT", "sunrise": 1661834187, "sunset": 1661882248 },
-            "timezone": 7200,
-            "id": 3163858,
-            "name": "Zocca",
-            "cod": 200
-        }
-    )
+    const [cityData, setCityData] = useState()
 
-    // const fetchData = () => {
-    //     //API
+    const fetchData = () => {
 
-    //     //https://api.openweathermap.org/data/2.5/weather?lat={coord.lat}&lon={coord.lon}&appid={8ee633956bad6ae1965b557a94ecfcba}&units=metric&lang=he
-    // }
+        axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+            params: {
+                lat: coord.lat,
+                lon: coord.lon,
+                appid: '8ee633956bad6ae1965b557a94ecfcba',
+                units: 'metric',
+                lang: 'he'
+            }
+        })
+            .then(response => {
+                setCityData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching the weather data:', error);
+            });
+    }
 
-    // useEffect(() => {
-    //     const res = fetchData()
-    //     setCityData(res) 
-    // }, []);
+    useEffect(() => {
+        fetchData()
+    }, []);
 
     return (<>
 
         {cityData ?
-        <CardTemplate
-            name={cityData.name}
-            descripthion={cityData.weather[0].description}
-            temp={cityData.main.temp}
-            feels_like={cityData.main.feels_like}
-            humidity={cityData.main.humidity}
-            icon={cityData.weather[0].icon}
-        ></CardTemplate> : <h3>אין נתונים</h3>}
-       
+            <CardTemplate
+                name={cityData.name}
+                descripthion={cityData.weather[0].description}
+                temp={cityData.main.temp}
+                feels_like={cityData.main.feels_like}
+                humidity={cityData.main.humidity}
+                icon={cityData.weather[0].icon}
+            ></CardTemplate> : <h3>אין נתונים</h3>}
     </>);
 }
 
